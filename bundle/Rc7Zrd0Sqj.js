a7008770797c73c79c8e421147fe5aefa0c5f700
j-yarbrough <j-yarbrough@users.noreply.github.com>
2026-03-21 16:19:25 +0000

    deploy: 99f36e80e3111e99df1c22fea4f1fd6a5b2aaa5b

--- bundle/Rc7Zrd0Sqj.js
+++ bundle/Rc7Zrd0Sqj.js
@@ -1,3 +1,22 @@
+window.addEventListener('load',function () {
+    this.document.querySelectorAll('tab-arrows button').forEach((arrowButton) => {
+        arrowButton.addEventListener('click',arrowButtonTrigger);
+    })
+})
+function arrowButtonTrigger () {
+    var tabInterface = this.closest('tab-interface');
+    var numberOfTabs = tabInterface.querySelectorAll('[role="tab"]').length;
+    var currentTabIndex = tabInterface.querySelector('[role="tab"][aria-selected="true"]').getAttribute('id').replace('tab-','');
+    var tabIndexToClick = currentTabIndex;
+    switch (this.getAttribute('data-pn')) {
+        case 'p':  tabIndexToClick--; break;
+        case 'n': tabIndexToClick++; break;
+    }
+    if (tabIndexToClick == 0) tabIndexToClick = numberOfTabs;
+    if (tabIndexToClick > numberOfTabs) tabIndexToClick = 1;
+    tabInterface.querySelector(`#tab-${tabIndexToClick}`).click();
+    tabInterface.querySelector(`#tabpanel-heading-${tabIndexToClick}`).focus();
+}
 /*
  *   This content is licensed according to the W3C Software License at
  *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
